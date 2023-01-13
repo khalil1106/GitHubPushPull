8d3772e9e6f002a5dc09f8528a19afdc81ce0ab7
Khalil <122052503+khalil1106@users.noreply.github.com>
2023-01-12 20:04:44 -0500

    Update Khalil.java

--- GitHubPushPull/src/teamMembers/Khalil.java
+++ GitHubPushPull/src/teamMembers/Khalil.java
@@ -3,7 +3,9 @@ package teamMembers;
 public class Khalil {
 	
 	public static void main(String[] args) {
-		System.out.println("Khalil");
+		System.out.println("Khalil lilhK");
+		
+		System.out.println("Updated on January 12th, 2023 - 8:04PM");
 	}
 
 
